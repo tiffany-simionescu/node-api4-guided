5a1fc06ff9b6dfaa561b4ae04f55e30f3e5bf39a
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-01-16 22:34:23 -0500

    Moved server into index.js

--- api/server.js
+++ api/server.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const helmet = require('helmet');
+// const express = require('express');
+// const helmet = require('helmet');
 
-const apiRouter = require('./api-router.js');
+// const apiRouter = require('./api-router.js');
 
-const server = express();
+// const server = express();
 
-server.use(helmet());
+// server.use(helmet());
 
-server.use('/api', apiRouter);
+// server.use('/api', apiRouter);
 
-server.use((req, res, next) => {
-  console.log(`
-    ${new Date().toLocaleString()} - ${req.ip} - ${req.method} - ${req.url}
-  `)
-})
+// server.use((req, res, next) => {
+//   console.log(`
+//     ${new Date().toLocaleString()} - ${req.ip} - ${req.method} - ${req.url}
+//   `)
+// })
 
-server.get("/", (req, res) => {
-  res.json({
-    message: "Welcome to my API",
-    cohort: process.env.LAMBDA_SCHOOL,
-    secret: process.env.SUPER_SECRET_API_KEY
-  })
-})  
+// server.get("/", (req, res) => {
+//   res.status(200).json({
+//     message: "Welcome to my API",
+//     cohort: process.env.LAMBDA_SCHOOL,
+//     secret: process.env.SUPER_SECRET_API_KEY
+//   })
+// })  
 
-module.exports = server;
+// module.exports = server;
--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const server = require('./api/server.js');
+const express = require('express');
+const helmet = require('helmet');
 
+const apiRouter = require('./api/api-router');
+
+const server = express();
 const host = process.env.HOST || "127.0.0.1";
 const port = process.env.PORT || 8080;
+
+server.use(helmet());
+
+server.use('/api', apiRouter);
+
+server.use((req, res, next) => {
+  console.log(`
+    ${new Date().toLocaleString()} - ${req.ip} - ${req.method} - ${req.url}
+  `)
+})
+
+server.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to my API",
+    cohort: process.env.LAMBDA_SCHOOL,
+    secret: process.env.SUPER_SECRET_API_KEY
+  })
+})  
+// end of copied server
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://${host}:${port} ***\n`);
